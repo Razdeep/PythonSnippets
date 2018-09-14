0fbadf3c998c8bdbf706605f23a986e00e007b18
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-09-14 17:31:12 +0530

    Modified some files of SEP11

--- SEP11/11.py
+++ SEP11/11.py
@@ -1,2 +1,15 @@
 # Write the program to replace the text in a file. 
-# Your program should prompt the user to enter filename,old string, new string.
+# Your program should prompt the user to enter filename, old string, new string.
+
+fp=open('sample.txt')
+fileName=input('Enter filename ')
+oldString=input('Enter old string to be replaced ')
+newString=input('Enter new string ')
+text=fp.readlines()
+fp.close()
+for i in range(len(text)):
+    text[i]=text[i].replace(oldString,newString)
+fp=open('sample2.txt','w')
+for i in text:
+    fp.write(i)
+fp.close()
--- SEP11/12.py
+++ SEP11/12.py
@@ -1,1 +1,5 @@
-Write the program that counts the number of characters in a given line 	http://cs.Armstrong.edu/liang/Data/Lincoln.txt
+# Write the program that counts the number of characters in a given line 	http://cs.Armstrong.edu/liang/Data/Lincoln.txt
+# import urllib3
+# data=urllib3.urlopen('http://cs.Armstrong.edu/liang/Data/Lincoln.txt')
+# print(data)
+# @TODO
